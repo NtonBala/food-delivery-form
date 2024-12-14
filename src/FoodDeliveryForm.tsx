b3dd0eea52cb5453dc8d5561596b257a7f51bb55
Anton Balashov <balashov.anton.1983@gmail.com>
2024-12-14 20:13:51 +0200

    Reset FoodDeliveryForm to UI

--- src/FoodDeliveryForm.tsx
+++ src/FoodDeliveryForm.tsx
@@ -1,40 +1,18 @@
-import { useForm, FieldErrors } from 'react-hook-form';
-
 type FoodDeliveryFormType = {
   customerName: string;
   mobile: string;
 };
 
 export const FoodDeliveryForm = () => {
-  const { register, handleSubmit } = useForm<FoodDeliveryFormType>();
-
-  const onSubmit = (formData: FoodDeliveryFormType) => {
-    console.log(formData);
-  };
-
-  const onError = (errors: FieldErrors<FoodDeliveryFormType>) => {
-    console.log('validation errors:', errors);
-  };
-
   return (
-    <form autoComplete="off" onSubmit={handleSubmit(onSubmit, onError)}>
+    <form autoComplete="off">
       <div className="form-floating mb-3">
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Customer Name"
-          {...register('customerName', { required: 'Customer name is required' })}
-        />
+        <input type="text" className="form-control" placeholder="Customer Name" />
         <label>Customer Name</label>
       </div>
 
       <div className="form-floating mb-3">
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Mobile"
-          {...register('mobile', { required: 'Mobile number is required' })}
-        />
+        <input type="text" className="form-control" placeholder="Mobile" />
         <label>Mobile</label>
       </div>
 
