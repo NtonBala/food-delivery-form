32de6d4cb4236a0a89f3d16b3a78f36901e59e6a
Anton Balashov <balashov.anton.1983@gmail.com>
2024-12-15 16:48:36 +0200

    Register input fields with default value

--- src/FoodDeliveryForm.tsx
+++ src/FoodDeliveryForm.tsx
@@ -1,18 +1,22 @@
+import { useForm } from 'react-hook-form';
+
 type FoodDeliveryFormType = {
   customerName: string;
   mobile: string;
 };
 
 export const FoodDeliveryForm = () => {
+  const { register } = useForm<FoodDeliveryFormType>();
+
   return (
     <form autoComplete="off">
       <div className="form-floating mb-3">
-        <input type="text" className="form-control" placeholder="Customer Name" />
+        <input type="text" className="form-control" placeholder="Customer Name" {...register('customerName', { value: 'Fiona' })} />
         <label>Customer Name</label>
       </div>
 
       <div className="form-floating mb-3">
-        <input type="text" className="form-control" placeholder="Mobile" />
+        <input type="text" className="form-control" placeholder="Mobile" {...register('mobile')} />
         <label>Mobile</label>
       </div>
 
