fd38f0738a495334116ec90d88e22f54912f439b
Anton Balashov <balashov.anton.1983@gmail.com>
2024-12-17 16:31:28 +0200

    Validate FoodDeliveryForm

--- src/FoodDeliveryForm.tsx
+++ src/FoodDeliveryForm.tsx
@@ -25,7 +25,7 @@ export const FoodDeliveryForm = () => {
   };
 
   return (
-    <form autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+    <form autoComplete="off" noValidate onSubmit={handleSubmit(onSubmit)}>
       <RenderCount />
 
       <div className="row mb-2">
@@ -38,7 +38,25 @@ export const FoodDeliveryForm = () => {
 
         <div className="col">
           <div className="form-floating">
-            <input type="text" className="form-control" placeholder="Mobile" {...register('mobile')} />
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Mobile"
+              {...register('mobile', {
+                minLength: {
+                  value: 10,
+                  message: 'Must be 10 digits.',
+                },
+                maxLength: {
+                  value: 10,
+                  message: 'Must be 10 digits.',
+                },
+                required: {
+                  value: true,
+                  message: 'This field is required.',
+                },
+              })}
+            />
             <label>Mobile</label>
           </div>
         </div>
@@ -47,13 +65,29 @@ export const FoodDeliveryForm = () => {
       <div className="row mb-2">
         <div className="col">
           <div className="form-floating">
-            <input type="text" className="form-control" placeholder="Customer Name" {...register('customerName')} />
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Customer Name"
+              {...register('customerName', { required: 'This field is required.' })}
+            />
             <label>Customer Name</label>
           </div>
         </div>
+
         <div className="col">
           <div className="form-floating">
-            <input type="text" className="form-control" placeholder="Email" {...register('email')} />
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Email"
+              {...register('email', {
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, // ! allows empty string
+                  message: 'Incorrect email format.',
+                },
+              })}
+            />
             <label>Email</label>
           </div>
         </div>
